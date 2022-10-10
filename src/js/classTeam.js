0c966807437a57c31d8413313c6524f0b8e10614
HelgaRoosh <vulfina@mail.ru>
2022-10-10 23:51:36 +0500

    add iterator

--- src/js/classTeam.js
+++ src/js/classTeam.js
@@ -18,4 +18,20 @@ export default class Team {
   toArray() {
     return Array.from(this.members);
   }
+
+  [Symbol.iterator]() {
+    const characters = this.toArray();
+    return {
+      current: 0,
+      last: characters.length,
+      next() {
+        if (this.current < this.last) {
+          const element = characters[this.current];
+          this.current += 1;
+          return { done: false, value: element };
+        }
+        return { done: true };
+      },
+    };
+  }
 }
